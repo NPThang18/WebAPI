0480320407fd0fc1198d08882a2f283d7246a965
ThangNP <thangcly@gmail.com>
2023-04-30 22:15:05 +0700

    Add Project

---
+++ DotNet WebAPI Core/DotNet WebAPI Core/DAO/Data.cs
@@ -0,0 +1,13 @@
+﻿using DotNet_WebAPI_Core.Models;
+
+namespace DotNet_WebAPI_Core.DAO
+{
+    public class Data
+    {
+        public static List<Product> listProduct = new List<Product>()
+        {
+            new Product(Guid.NewGuid(), "Dell XPS", 28000),
+            new Product(Guid.NewGuid(), "Macbook Air M2", 29000),
+        };
+    }
+}
---
+++ DotNet WebAPI Core/DotNet WebAPI Core/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNet_WebAPI_Core.Models
+{
+    public class Product
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="name"></param>
+        /// <param name="price"></param>
+        public Product(Guid key, string name, int price)
+        {
+            this.Key = key;
+            this.Name = name;
+            this.Price = price;
+        }
+        /// <summary>
+        /// Product key
+        /// </summary>
+        public Guid Key { get; set; }
+        /// <summary>
+        /// Product name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Product Price
+        /// </summary>
+        public int Price { get; set; }
+    }
+}
